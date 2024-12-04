1f9683902a8ad02854aa53850553ebf687a1060a
nhinijja <nhi81924@gmail.com>
2024-12-04 19:07:22 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import trangchu from "./components/trangchu.vue";
+import navbar from "./components/narbar.vue";
+import footerClient from "./components/footer.vue";
+</script>
+
+<template>
+  <navbar />
+  <router-view></router-view>
+  <footerClient />
+</template>
---
+++ src/components/narbar.vue
@@ -0,0 +1,93 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light navbar-light">
+    <div class="container">
+      <router-link to="/" class="navbar-brand">
+        Burger <span>King</span>
+      </router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Trang chủ</router-link>
+          </li>
+          <!-- Hiển thị liên kết "Quản lý bài viết" và "Trang cá nhân" nếu đã đăng nhập -->
+
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link class="nav-link" to="/chef"> Đầu bếp</router-link>
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link class="nav-link" to="/menu"> Menu</router-link>
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link class="nav-link" to="/contact">
+              Liên hệ với chúng tôi</router-link
+            >
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link class="nav-link" to="/managerBlog"
+              >Quản lý bài viết</router-link
+            >
+          </li>
+          <li v-if="isLoggedIn" class="nav-item">
+            <router-link class="nav-link" to="/profile"
+              >Trang cá nhân</router-link
+            >
+          </li>
+          <!-- Hiển thị "Đăng nhập" nếu chưa đăng nhập -->
+          <li v-if="!isLoggedIn" class="nav-item">
+            <router-link class="nav-link" to="/login">Đăng nhập</router-link>
+          </li>
+          <!-- Hiển thị "Đăng xuất" nếu đã đăng nhập -->
+          <li v-if="isLoggedIn" class="nav-item">
+            <a class="nav-link" href="#!" @click.prevent="logout">Đăng xuất</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { computed } from "vue";
+
+// const isLoggedIn = ref(false);
+const router = useRouter();
+const store = useStore();
+
+const isLoggedIn = computed(() => store.getters.getLoginStatus);
+const userInfo = computed(() => store.getters.getUserInfo);
+
+const logout = () => {
+  localStorage.removeItem("userInfo");
+  localStorage.removeItem("isLoggedIn");
+  isLoggedIn.value = false;
+  window.location.reload();
+
+  console.log("Đăng xuất người dùng");
+  localStorage.removeItem("data");
+  this.$router.push("/login"); // Điều hướng đến trang login sau khi đăng xuất
+};
+</script>
+
+<style scoped>
+.nav-link {
+  color: rgb(24, 25, 24) !important; /* Đặt màu đen cho tất cả các liên kết */
+}
+
+.nav-link:hover {
+  color: #4322ff; /* Thêm hiệu ứng màu khi hover nếu muốn */
+  text-decoration: underline; /* Gạch chân khi di chuột nếu cần */
+}
+</style>
